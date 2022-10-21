2d59cf41329fce85384510ade9f05c6889c1b9e3
hyunjin <hyunjin5697@yonsei.ac.kr>
2022-10-21 16:19:11 +0900

    파일 위치 이동

--- class4/15652.js
+++ class4/15652.js
@@ -6,14 +6,17 @@ let input = fs
     .split("\n");
 let [n, m] = input[0].split(' ').map((e) => +e);
 
-let arr = input[1].split(' ').map((e) => +e).sort((a, b) => a - b)
+let arr = [];
+for (let i = 1; i <= n; i++) {
+    arr.push(i);
+}
 
 const getPermuations = (arr, selectNum) => {
     let res = [];
     if (selectNum === 1) return arr.map((e) => [e]);
 
     arr.forEach((fixed, idx, origin) => {
-        let newArr = [...origin];
+        let newArr = [...origin.slice(idx)];
         let permutations = getPermuations(newArr, selectNum - 1);
         let attached = permutations.map((e) => [fixed, ...e]);
         res.push(...attached);
@@ -22,6 +25,4 @@ const getPermuations = (arr, selectNum) => {
 }
 
 let res = getPermuations(arr, m);
-res = res.map((e) => e.sort((a, b)=>a-b).join(' '));
-res = new Set(res);
-res.forEach((e) => e && console.log(e));
+res = res.map((e) => console.log(e.sort((a, b) => a - b).join(' ')));
--- class4/15658.js
+++ class4/15658.js
@@ -6,17 +6,14 @@ let input = fs
     .split("\n");
 let [n, m] = input[0].split(' ').map((e) => +e);
 
-let arr = [];
-for (let i = 1; i <= n; i++) {
-    arr.push(i);
-}
+let arr = input[1].split(' ').map((e) => +e).sort((a, b) => a - b)
 
 const getPermuations = (arr, selectNum) => {
     let res = [];
     if (selectNum === 1) return arr.map((e) => [e]);
 
     arr.forEach((fixed, idx, origin) => {
-        let newArr = [...origin];
+        let newArr = [...origin.slice(idx)];
         let permutations = getPermuations(newArr, selectNum - 1);
         let attached = permutations.map((e) => [fixed, ...e]);
         res.push(...attached);
@@ -25,6 +22,4 @@ const getPermuations = (arr, selectNum) => {
 }
 
 let res = getPermuations(arr, m);
-res = res.map((e) => e.sort((a, b) => a - b).join(' '));
-res = new Set(res);
-res.forEach((e) => e && console.log(e));
+res = res.map((e) => console.log(e.sort((a, b)=>a-b).join(' ')));
