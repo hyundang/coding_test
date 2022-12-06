8ba98e0d9f80897488ada1f2073793f837aee23d
hyunjin <hyunjin5697@gmail.com>
2022-12-06 21:43:24 +0900

    12/06

---
+++ class4/10830.js
@@ -0,0 +1,4 @@
+let fs = require("fs");
+// '/dev/stdin'
+let input = fs.readFileSync("예제.txt", "utf-8").trim().split("\n");
+
--- class4/14938.js
+++ class4/14938.js
@@ -4,77 +4,41 @@ let input = fs.readFileSync("예제.txt", "utf-8").trim().split("\n");
 
 const [n, m, r] = input[0].split(" ").map(Number);
 const items = [0, ...input[1].split(" ").map(Number)];
-let adj = Array.from({ length: n + 1 }, () => new Array());
+
+let adj = Array.from({ length: n + 1 }, () => new Array(n + 1).fill(Infinity));
 input.slice(2).forEach((info) => {
   const [u, v, w] = info.split(" ").map(Number);
-  adj[u].push([v, w]);
-  adj[v].push([u, w]);
+  adj[u][v] = w;
+  adj[v][u] = w;
 });
 
-// min-heap
-class Heap {
-  constructor() {
-    this.heap = [];
-  }
-
-  push(newValue) {
-    const heap = this.heap;
-    heap.push(newValue);
-    let index = heap.length - 1,
-      parent = Math.floor((index - 1) / 2);
-    while (index > 0 && heap[parent][1] > heap[index][1]) {
-      [heap[parent], heap[index]] = [heap[index], heap[parent]];
-      index = parent;
-      parent = Math.floor((index - 1) / 2);
+for (let k = 1; k <= n; k++) {
+  for (let i = 0; i <= n; i++) {
+    for (let j = 0; j <= n; j++) {
+      if (i === j) continue;
+      if (
+        adj[i][k] !== 0 &&
+        adj[k][j] !== 0 &&
+        adj[i][j] > adj[i][k] + adj[k][j]
+      )
+        adj[i][j] = adj[i][k] + adj[k][j];
     }
-  }
-
-  pop() {
-    const heap = this.heap;
-    if (heap.length <= 1) return heap.pop();
-    const ret = heap[0];
-    heap[0] = heap.pop();
-    let index = 0;
-    while (1) {
-      let left = index * 2 + 1,
-        right = index * 2 + 2;
-      if (left >= heap.length) break;
-      let next = index;
-      if (heap[left][1] > heap[next][1]) next = left;
-      if (right < heap.length && heap[next][1] > heap[right][1]) next = right;
-      if (next === index) break;
-      [heap[index], heap[next]] = [heap[next], heap[index]];
-      index = next;
-    }
-    return ret;
   }
 }
 
-function dijkstra(start) {
-  let visited = new Array(n + 1).fill(-1);
-  let pq = new Heap();
-  pq.push([start, 0]);
-  visited[start] = 0;
-  while (pq.heap.length > 0) {
-    const [cur_v, cur_w] = pq.pop();
-    adj[cur_v].forEach((next) => {
-      const [v, w] = next;
-      if (visited[v] === -1 || visited[v] > visited[cur_v] + w) {
-        visited[v] = visited[cur_v] + w;
-        pq.push([v, visited[v]]);
-      }
-    });
-  }
-
-  let cnt = 0;
-  for (let i = 1; i <= n; i++) {
-    if (visited[i] <= m) cnt += items[i];
-  }
-  return cnt;
-}
-
-let answer = [];
+let max = 0;
 for (let i = 1; i <= n; i++) {
-  answer.push(dijkstra(i));
+  let cnt = 0;
+  for (let j = 1; j <= n; j++) {
+    if (i === j) {
+      cnt += items[i];
+      continue;
+    }
+    if (adj[i][j] <= m) {
+      cnt += items[j];
+    }
+  }
+  if (max < cnt) max = cnt;
 }
-console.log(Math.max(...answer));
+
+console.log(max);
